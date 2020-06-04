8b7a733454784c468d642ee32a73dc8b81ce84a4
minhtn143 <minhtn143@gmail.com>
2020-06-04 14:28:12 +0700

    create route in routes/api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/customers', 'CustomerController@index')->name('customers.all');
+Route::get('/customers/{customerId}', 'CustomerController@show')->name('customers.show');
+Route::post('/customers', 'CustomerController@store')->name('customers.store');
+Route::put('/customers/{customerId}', 'CustomerController@update')->name('customers.update');
+Route::delete('/customers/{customerId}', 'CustomerController@destroy')->name('customers.destroy');
